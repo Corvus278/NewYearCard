6506639c994b32774061a574d3650bddfebdb8cf
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2022-01-01 01:38:28 +0300

    Сделал основной функционал открытки

--- js/index.js
+++ js/index.js
@@ -62,12 +62,59 @@ function index() {
   mainBtn()
 }
 
+// Осторожно, говонокод, вермени было мало)
+function newYearCard() {
+  let clickCount = 0
+  const btn = document.querySelector('.tree-button')
+  const treeNoLight = document.querySelector('.tree--no-light')
+  const treeLight = document.querySelector('.tree--light')
+  const textContent = document.querySelector('.text-cont')
+  const geraltImg = document.querySelector('.geralt-img')
+
+  function btnChangeText() {
+    btn.textContent = data.treeButtonText[clickCount]
+  }
+
+  function btnChangeColor() {
+    btn.style.background = data.treeButtonColor[clickCount]
+  }
+
+  function lightTree() {
+    treeNoLight.classList.add('none')
+    treeLight.classList.remove('none')
+  }
+
+  function begin() {
+    lightTree()
+
+    setTimeout(() => {
+      textContent.classList.remove('none')
+    }, 1000)
+
+    setTimeout(() => {
+      geraltImg.style.transform = 'none'
+    }, 2000)
+  }
+
+  function click() {
+    if (clickCount === 5) {
+      begin()
+      btn.classList.add('none')
+    }
+    btnChangeText()
+    btnChangeColor()
+    clickCount++
+  }
+
+  click()
+  btn.addEventListener('click', click)
+}
+
 (() => {
-  console.log(isPage('index'))
   if (isPage('index')) {
     index()
     console.log(1)
-  } else if (isPage('present')) {
-    present()
+  } else if (isPage('newYearCard')) {
+    newYearCard()
   }
 })()
