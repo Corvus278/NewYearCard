c0b8f093f56640aeeb5c120274f7e46da3ca76bf
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2022-01-01 04:58:44 +0300

    Добавил одного получателя)

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const data = {
+  mainButtonText: {
+    0: 'Нажми!',
+    1: 'Мимо :)',
+    2: 'Опять!',
+    3: 'Я здесь)',
+    4: 'Ладно :)',
+  },
+
+  treeButtonText: {
+    0: 'Зажечь ёлку!',
+    1: 'Раз...',
+    2: 'Два...',
+    3: 'Три...',
+    4: 'Гори!',
+  },
+
+  treeButtonColor: {
+    0: '#7effd4',
+    1: '#FFD276',
+    2: '#FFAD05',
+    3: '#FF7976',
+    4: '#FF4944',
+  },
+
+  createOwner(owner) {
+    if (owner === 'mash') {
+      return data.owner = 'Маша'
+    }
+  }
+}
